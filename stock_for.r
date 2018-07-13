f9ef358c68608c3cf56c4534e1d79d86c2dcf6fe
Shashank Shekhar <shekhar.shashank1122@gmail.com>
2018-07-14 02:26:35 +0530

    Update stock_for.r

--- stock_for.r
+++ stock_for.r
@@ -2,7 +2,7 @@ library(quantmod)
 library(forecast)
 getSymbols(Company)
 write.zoo(Company, file="Company.csv", sep=",")
-company=read.csv("Company.csv",header = T)/#taking the input
+company=read.csv("Company.csv",header = T)#taking the input
 head(company)
 company.ts=ts(rev(company[,]),start=c(2014,8),freq=3)#quarterly
 company.ts
